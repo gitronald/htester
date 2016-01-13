8505f98bb8a6c40520330f08ad1738022790c4be
Ronald Robertson <gitronald@users.noreply.github.com>
2016-01-13 01:10:52 -0800

    Add in function comments

--- R/htest_data_frame.R
+++ R/htest_data_frame.R
@@ -4,28 +4,28 @@ htest_data_frame <- function(htest.list) {
   # REPLACES ks.testm and cor.testm
   #
   data1 <- htest.list
-  item.lengths <- sapply(data1, length) #Find max list length
-  col.names <- names(data1[[which.max(item.lengths)]]) #Take col names from largest sub list
+  item.lengths <- sapply(data1, length) # Find max list length
+  col.names <- names(data1[[which.max(item.lengths)]]) # Take col names from largest sub list
 
   if(sum(grepl("conf.int", col.names)) > 0) {
     for (i in 1:length(data1)) {
       data1[[i]][["ci.min"]] <- data1[[i]][["conf.int"]][1]
       data1[[i]][["ci.max"]] <- data1[[i]][["conf.int"]][2]
-      data1[[i]][["conf.int"]] <- NULL
+      data1[[i]][["conf.int"]] <- NULL    # Remove column
     }
-    col.names <- col.names[!col.names == "conf.int"]
+    col.names <- col.names[!col.names == "conf.int"]    # Remove name from name index
     col.names <- c(col.names, "ci.min", "ci.max")
   }
 
   # Create data structure
-  stats.data <- setNames(data.frame(matrix(ncol = length(col.names),
+  data_frame <- setNames(data.frame(matrix(ncol = length(col.names),
                                            nrow = length(data1))),
                          nm = col.names)
 
   # Fill data structure by row
   for(i in col.names) {
-    stats.data[, i] <- sapply(seq_along(data1), function(x) data1[[x]][[i]])
+    data_frame[, i] <- sapply(seq_along(data1), function(x) data1[[x]][[i]])
   }
 
-  return(stats.data)
+  return(data_frame)
 }
