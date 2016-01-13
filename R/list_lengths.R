f4b9d09e663ea2ce363bebf211c347a33cacc6d6
Ronald Robertson <gitronald@users.noreply.github.com>
2016-01-13 02:27:38 -0800

    Add simplify argument to sapply for overOne data

--- R/list_lengths.R
+++ R/list_lengths.R
@@ -27,7 +27,7 @@ list_lengths <- function(data1, overOne = TRUE) {
   )
 
   if (overOne) {
-    max <- sapply(seq_along(data1), function(x) which(data1[[x]] > 1))
+    max <- sapply(seq_along(data1), function(x) which(data1[[x]] > 1), simplify=F)
     list1 <- list(data1,
                   max)
     names(list1) <- c("length", "overOne")
