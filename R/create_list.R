665e6633329372a1b01e54d529e6b4e6674ddde0
Ronald Robertson <gitronald@users.noreply.github.com>
2016-01-14 14:19:50 -0800

    Add create_list script and documentation

---
+++ R/create_list.R
@@ -0,0 +1,26 @@
+#' Create a list of named lists
+#'
+#' Use \code{create_list} to create a list of named lists, each of length \code{list.lengths}.
+#'
+#' @param list.names a string vector containing list names
+#' @param list.lengths a numeric value that determines the length of each \code{list.names} list
+#'
+#' @return
+#'    Returns a list of named lists, each of length \code{list.lengths}.
+#' @export
+#'
+#' @examples
+#' # Collect list names
+#' htest.names <- c("data.cor.test.pearson",
+#'                  "data.ks.test",
+#'                  "data.wilcox.test.wilcox",
+#'                  "data.t.test")
+#'
+#' # Generate lists, each of length 4
+#' create_list(htest.names, 4)
+#'
+create_list <- function(list.names, list.lengths) {
+  # Generate list of lists named after list.names, each containing a list of length list.lengths
+  new.list <- sapply(list.names, function(x) lapply(x, function(x) vector("list", length = list.lengths)))
+  return(new.list)
+}
