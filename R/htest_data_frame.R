963f380330be020c7384e04763c3dfae690ab516
Ronald Robertson <gitronald@users.noreply.github.com>
2016-01-15 11:17:34 -0800

    Fix errors thrown on double value columns

--- R/htest_data_frame.R
+++ R/htest_data_frame.R
@@ -19,21 +19,31 @@ htest_data_frame <- function(htest.list) {
   item.lengths <- sapply(data1, length) # Find max list length
   col.names <- names(data1[[which.max(item.lengths)]]) # Take col names from largest sub list
 
-#
-#   ll <- list_lengths(data1)
-#   if (max(sapply(ll$overOne, length)) > 1) {
-#     sapply(seq_along(d), function(x) sapply(seq_along(ll$overOne), function(y) names(d[[x]][ll$overOne[[y]]])), simplify = F)
-#   }
+  # Map list lengths
+  ll <- list_map_lengths(data1)
+  empty.cols <- names(which(sapply(ll, sum) == 0))      # Find all empty columns
+  multi.cols <- names(which(apply(ll, 2, max) > 1))     # Find all columns containing > 1 values
+  col.names <- col.names[!(col.names %in% empty.cols)]  # Remove empty cols from name index
 
-
-  if(sum(grepl("conf.int", col.names)) > 0) {
-    for (i in 1:length(data1)) {
-      data1[[i]][["ci.min"]] <- data1[[i]][["conf.int"]][1]
-      data1[[i]][["ci.max"]] <- data1[[i]][["conf.int"]][2]
-      data1[[i]][["conf.int"]] <- NULL    # Remove column
+  if(length(multi.cols > 0)) {
+    if("conf.int" %in% multi.cols) {
+      for (i in 1:length(data1)) {
+        data1[[i]][["ci.min"]] <- data1[[i]][["conf.int"]][1]
+        data1[[i]][["ci.max"]] <- data1[[i]][["conf.int"]][2]
+        data1[[i]][["conf.int"]] <- NULL    # Remove column
+      }
+      col.names <- col.names[!col.names == "conf.int"]    # Remove name from name index
+      col.names <- c(col.names, "ci.min", "ci.max")
     }
-    col.names <- col.names[!col.names == "conf.int"]    # Remove name from name index
-    col.names <- c(col.names, "ci.min", "ci.max")
+    if("estimate" %in% multi.cols) {
+      for (i in 1:length(data1)) {
+        data1[[i]][["estimate.x"]] <- data1[[i]][["estimate"]][1]
+        data1[[i]][["estimate.y"]] <- data1[[i]][["estimate"]][2]
+        data1[[i]][["estimate"]] <- NULL    # Remove column
+      }
+      col.names <- col.names[!col.names == "estimate"]    # Remove name from name index
+      col.names <- c(col.names, "estimate.x", "estimate.y")
+    }
   }
 
   # Create data structure
